5847f6088503472bc31b4b1909b541cab0d67d74
github username <github email address>
2019-05-29 19:43:24 -0400

    done

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -4,27 +4,69 @@ import PokemonCard from './PokemonCard'
 
 export default class PokemonCollection extends Component {
 
+
+//if this function turns back false dont push
+// checkArr=(arr,pokemon)=>{
+//   let newObj = pokemon
+//   arr.forEach(pokemonObj=>{
+//     debugger
+//
+//   })
+//
+// debugger
+// }
+
+
 setPokemonArr=()=>{
   let pokemonArr=[]
+
+
   if(this.props.region ==="Your Collection"){
+    let newArr=[]
+    let newObj ={}
+
     this.props.pokemon.forEach(arr=>
-      arr.pokemons.forEach(pokemon=>pokemonArr.push(pokemon))
-    )
+      arr.pokemons.forEach(pokemon=>{
+        if(newObj[pokemon.name]===undefined){
+          newObj[pokemon.name]=true
+          newArr.push(pokemon)
+
+        }
+        else{}
+      }))
+
+
+
+      // newArr.forEach(pokemon=>{
+      //   if(pokemonArr.length===0){pokemonArr.push(pokemon)}
+      //   else{
+      //     pokemonArr.forEach(obj=>{
+      //       debugger
+      //       if(obj.id!==pokemon.id){
+      //         pokemonArr.push(pokemon)
+      //       }
+      //     })
+      //   }
+      // }
+      pokemonArr=newArr
   }else{
     pokemonArr = [...this.props.pokemon]
   }
+  console.log("Thank you brenden")
     return pokemonArr.filter(pokemon=>pokemon.name.includes(this.props.searchTerm))
 }
 
 
   renderPokemonCards = () => {
     const pokemonArr = this.setPokemonArr();
+
     return pokemonArr.map((poke, index) => {
       return <PokemonCard capitalizeFirstLetterOfName={this.props.capitalizeFirstLetterOfName} capitalizeFirstLetterOfType={this.props.capitalizeFirstLetterOfType} key={poke.id} i={index + 1} pokemon={poke} />
     })
   }
 
   render() {
+
     return (
       <div>
         <div className="region-header">
